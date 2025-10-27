de9a87f8fc1671dca98458cb07f3ece5430b616b
cortisandrew <andrew.cortis@mcast.edu.mt>
2025-10-27 10:00:05 +0100

    Added extra methods in Driver.

--- SMC_Simulation_Example/MonteCarloDriver.cs
+++ SMC_Simulation_Example/MonteCarloDriver.cs
@@ -9,24 +9,30 @@ namespace SMC_Simulation_Example
     public class MonteCarloDriver
     {
         private ISimulation _simulator;
-        private List<double> _results = new List<double>();
+        private List<double> _yi = new List<double>();
 
         public MonteCarloDriver(ISimulation simulator)
         {
             _simulator = simulator;
         }
 
-        public void RunSimulations(int n)
+        public void RunSimulations(long n)
         {
-            _results.Clear();
-            for (int i = 0; i < n; i++)
+            // TODO: Known issue! List cannot accept length greater than int.MaxValue
+            if (n > int.MaxValue)
             {
-                _results.Add(_simulator.Simulate());
+                throw new OverflowException("Number of simulations exceeds int.MaxValue");
+            }
+
+            _yi.Clear();
+            for (long i = 0; i < n; i++)
+            {
+                _yi.Add(_simulator.Simulate());
             }
         }
 
         public double EstimatedMean()         {
-            if (_results.Count == 0)
+            if (_yi.Count == 0)
             {
                 throw new InvalidOperationException("No simulations have been run.");
             }
@@ -34,13 +40,69 @@ namespace SMC_Simulation_Example
             double mean = 0.0;
             // mean = _results.Average();
 
-            for (int i = 0; i < _results.Count; i++)
+            // TODO: Known issue! List cannot accept length greater than int.MaxValue
+            for (int i = 0; i < _yi.Count; i++)
             {
-              mean += _results[i];
+              mean += _yi[i];
             }
-            mean /= _results.Count;
+            mean /= _yi.Count;
 
             return mean;
+        }
+
+        /// <summary>
+        /// sigma^2 hat
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public double EstimatedVariance()
+        {
+            if (_yi.Count == 0)
+            {
+                throw new InvalidOperationException("No simulations have been run.");
+            }
+
+            double mean = EstimatedMean();
+            double variance = 0.0;
+            for (int i = 0; i < _yi.Count; i++)
+            {
+                variance += (_yi[i] - mean) * (_yi[i] - mean);
+            }
+            variance /= (_yi.Count - 1); // Sample variance
+            return variance;
+        }
+
+        /// <summary>
+        /// sigma hat
+        /// </summary>
+        /// <returns></returns>
+        public double EstimatedStandardDeviation()
+        {
+            double variance = EstimatedVariance();
+            return Math.Sqrt(variance);
+        }
+
+        /// <summary>
+        /// Estimated (because we are estimating the variance and std deviation
+        /// </summary>
+        /// <returns></returns>
+        public double RMSE()
+        {
+            return EstimatedStandardDeviation() / Math.Sqrt(_yi.Count);
+        }
+
+        public long EstimateRequiredSimulations(double RMSE_Target)
+        {
+            // TODO: Check for overflows when converting to long
+            double variance = EstimatedVariance();
+            double n_required_double = variance / (RMSE_Target * RMSE_Target);
+            if (n_required_double > long.MaxValue)
+            {
+                throw new OverflowException("Estimated required simulations exceed long.MaxValue");
+            }
+
+            long n_required = (long)Math.Ceiling(variance / (RMSE_Target * RMSE_Target));
+            return n_required;
         }
     }
 }
--- SMC_Simulation_Example/Program.cs
+++ SMC_Simulation_Example/Program.cs
@@ -3,15 +3,22 @@ using SMC_Simulation_Example;
 
 Console.WriteLine("Hello, World!");
 
-int[] problemSizes = new int[] { 100, 10000, 1000000 };
+long[] problemSizes = new long[] { 100, 10000, 1000000 };
 
 ISimulation simulation = new CoinGameSimulation();
 MonteCarloDriver driver = new MonteCarloDriver(simulation);
 
-foreach (int n in problemSizes)
+foreach (long n in problemSizes)
 {
     // Run the simulation MANY times
     driver.RunSimulations(n);
     double estimatedMean = driver.EstimatedMean();
     Console.WriteLine($"Estimated expected winnings (or loss) for {n} simulations: {estimatedMean}");
+    Console.WriteLine($"Estimated variance for {n} simulations: {driver.EstimatedVariance()}");
+    Console.WriteLine($"Estimated RMSE for {n} simulations: {driver.RMSE()}");
+    Console.WriteLine("-----------------------------------------");
+    Console.WriteLine();
 }
+
+double RMSE_target = 0.00001;
+Console.WriteLine($"Estimated number of simluations to obtain an RMSE of {RMSE_target} is {driver.EstimateRequiredSimulations(RMSE_target)}");
